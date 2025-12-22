1e91c2da5471d44abdc334e7d38606f17e371ff8
betacoder7 <webdesigner2502@gmail.com>
2025-12-22 19:07:33 +0530

    preloader change

--- assets/js/api-config.js
+++ assets/js/api-config.js
@@ -1,7 +1,6 @@
 const API_CONFIG = {
-    API_BASE: 'https://glyptic.in/api',
+    API_BASE: 'https://api.glyptic.in/api',
     UPLOAD_BASE: 'https://api.glyptic.in'
 }
 
 window.API_CONFIG = API_CONFIG;
-export default API_CONFIG;
